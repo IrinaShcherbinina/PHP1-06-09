2fc1cba21ad508d8866a6df7dff08adfdcda0d68
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-09-09 17:39:42 +0300

    lesson1

---
+++ partoftask4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>lesson1task4</title>
+    <meta name="title" content="lesson1task4" />
+</head>
+
+<body>
+    <!--Используя имеющийся HTML-шаблон, сделать так, чтобы главная страница генерировалась через PHP. Создать блок переменных в начале страницы. Сделать так, чтобы h1, title и текущий год генерировались в блоке контента из созданных переменных.-->
+    <div id="main">
+        <header>
+            <h1>Lesson1task4</h1>
+            <h2 class="change">Hew header2</h2>
+            <h3 class="change">Hew header3</h3>
+            <h3 class="change">Hew header4</h3>
+            <h3 class="change">Hew header5</h3>
+            <form>
+                <p>Выберите дату: <input type="date" name="calendar"></p>
+            </form>
+        </header>
+    </div>
+</body>
+
+</html>
