ffce34043817b57097c5e354c73cfc7fb5da91fe
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-09-20 11:41:49 +0300

    Update removeDir.php

--- lesson4/removeDir.php
+++ lesson4/removeDir.php
@@ -1,32 +1,20 @@
 <?php
-
-//удаление непустой директории
-function removeDir($path)
-{
-    //если аргумент - это папка, то отбрасывем точки,
+function removeDir($path){
     if (is_dir($path) && $path != '.' && $path != '..') {
-        //открываем ее
-        $dir = opendir($path);
-        //пока в ней что-то есть (кроме точек), запускаем рекурсивно нашу функцию
-        while ($file = readdir($dir)) {
-            if ($file != '.' && $file != '..') {
-                removeDir($path . '/' . $file);
-            }
-        }
-        //закрываем поток
-        closedir($dir);
-        //удаляем папку с выводом сообщения
-        rmdir($path);
-        echo "dir <b> {$path} </b> is delete!<br>";
-    //если аргумент - это файл, то удаляем его
-    } elseif
-    (is_file($path)) {
+         $dir = opendir($path);
+             while ($file = readdir($dir)) {
+                if ($file != '.' && $file != '..') {
+                    removeDir($path . '/' . $file);
+                    }
+                }
+          closedir($dir);
+          rmdir($path);
+          echo "dir <b> {$path} </b> is delete!<br>";
+    } elseif is_file($path)) {
         echo "file {$path} is delete <br>";
         unlink($path);
-    //в случае ошибки (аргумент не существует)
     } else {
         return print "incorrect argument: '{$path}'";
     }
 }
-
 ?>
