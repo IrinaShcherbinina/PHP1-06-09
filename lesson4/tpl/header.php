c6247b841a16fc14056ac8c148eeca452f18e0a0
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-09-20 11:33:42 +0300

    lesson4

---
+++ lesson4/tpl/header.php
@@ -0,0 +1,21 @@
+<?php include_once 'settings/settings.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+<!--
+<!doctype html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ 
+							
+-->
+  
+ 
