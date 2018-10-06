9b12e4111d85032040979f0976990f90a40c8be7
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-10-06 19:53:39 +0300

    lesson08

---
+++ lesson08/templates/users/_basket_items.php
@@ -0,0 +1,20 @@
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+   	<meta charset="UTF-8">
+   </head>
+   <body>
+   	 	<div id="product-{{ID}}" class="panel panel-default">
+    <div class="panel-body">
+    	<p><b>Товар:</b> <a href="./index.php?r=products/view&id={{ID}}">{{NAME}}</a></p>
+        <p><b>Артикул:</b> {{ARTICUL}}</p>
+        <p><b>Артикул:</b> {{COST}} р.</p>
+        <p><b>Изображение:</b></p>
+        <img src="{{IMG_THUMB_PATH}}" alt="{{NAME}}">
+        <p class="text-right">{{EDIT}} {{DELETE}}</p>
+    </div>
+</div>
+
+   </body>
+   </html>
+   
